1856fdf3cb268e1023acf74223d94d2e10593523
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-08-15 19:00:45 -0300

    Addition of the BarsManager class

---
+++ source/baresManager.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "./baresManager.h"
+#include "../EBNF_basic/source/include/parser.h"
+
+
+int main() {
+  BaresManager BM; // an instance of class BaresManager
+}
---
+++ source/baresManager.h
@@ -0,0 +1,8 @@
+#ifndef _BARESMANAGER_H_
+#define _BARESMANAGER_H_
+
+class BaresManager {
+  
+}
+
+#endif
